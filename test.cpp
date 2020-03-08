54b92784c54a52cd8ee07a1aa6b5d699f9adc1fb
cccsar <15-11295@usb.ve>
2020-03-08 13:15:58 -0400

    saving for bitmap

--- test.cpp
+++ test.cpp
@@ -8,8 +8,10 @@
 #include<queue>
 #include<utility>
 #include<climits> 
-#include<list>
+#include<unordered_set>
 
+
+using namespace std; 
 
 #define FOR(u,l) for(int i=l; i<u; i++) 
 #define ROF(l,u) for(int i=u; i>l-1; i--) 
@@ -17,27 +19,32 @@
 #define rii(c,cc) scanf("%d %d", &c, &cc)
 #define riii(c,cc,ccc) scanf("%d %d %d", &c, &cc, &ccc)
 #define rw(s) scanf("%s",&s)
-#define vi vector< int > 
-#define vii vector< pair<int, int> >  
-#define viii vector< int, pair<int, int> >
-#define pi pair <int, int> 
-#define pii pair <int, pair<int, int> > 
-#define adl vector< vi > 
-#define wadl vector< vii > 
 
-
+typedef vector< int > vi; 
+typedef vector< pair<int, int> >  vii; 
+typedef vector< int, pair<int, int> >viii; 
+typedef pair <int, int> pi; 
+typedef pair <int, pair<int, int> > pii; 
+typedef vector< vi > adl; 
+typedef vector< vii > wadl; 
 	
-using namespace std; 
 
-int main() { 
+int main() 
+{
 	int i_; 
-	list<int> hola; 
-	for(i_=0; i_<10 ; i_++) 
-		hola.push_front(i_); 	
+	unordered_set< pi > hola; 
+	pi dummie; 
 
-	for(int i : hola )   
-		cout<<i<<" "; 	
-	cout<<endl; 
+	for(i_=0; i_<10 ; i_++) {
+		dummie.first = i_; 	
+		dummie.second = i_+1; 	
+		hola.insert( dummie ) ;
+	}	
+
+	hola.insert( {4,5} );
+
+	for(pi k : hola ) 
+		cout<<k.first<<" "<<k.second<<endl; 
 
 	return 0; 
 }
