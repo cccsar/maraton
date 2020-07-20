1d6f12f87dbfb8e8fc19eefe2f494b440db3ac95
cccsar <15-11295@usb.ve>
2020-07-20 11:25:06 -0400

    again, removing unnecesary

--- nsteps.hs
+++
@@ -1,9 +0,0 @@
-
-nsteps :: (Integral a, Show a) => (a, a) -> String 
-nsteps (x, y) 
-	| x == y && even x = show (2 * x)
-	| x == y && odd x = show (2 * x - 1)
-	| x == y + 2 && even x = show (x + y)
-	| x == y + 2 && odd y = show (x + y - 1)
-	| otherwise = "No Number"
-
--- offside.hs
+++
@@ -1,7 +0,0 @@
-listMin :: (Ord a) => [a] -> a
-listMin (x:xs)
-	| length xs == 1 = min x y 
-	| otherwise = min x (listMin xs)
-	where 	y = head xs
-
-
