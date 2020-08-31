52304c63685094fe9410d67a609697b9e7a36114
cccsar <15-11295@usb.ve>
2020-08-30 21:23:58 -0400

    nada

---
+++ uva/uva621.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#include <string.h>
+
+#define MAXS 10000
+
+int resp[MAXS]; 
+
+
+char check( char wd[MAXS] ) { 
+	int st = 0; 
+	for (int i=0; i<strlen(wd); i++) {
+	}
+}
+
+int main () { 
+	int n; 
+	char wd[MAXS];
+
+	scanf("%d",&n); 
+
+	for (int i=0; i<n; i++) { 
+		scanf("%s",wd); 	
+	}
+}
