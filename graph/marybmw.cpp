669c88a14afad9565e44ac1f0b9ccd5f00178dc7
cccsar <15-11295@usb.ve>
2020-09-18 21:46:42 -0400

    tried a prim approach to marybmw, but n^2 is a bitch

--- graph/marybmw.cpp
+++ graph/marybmw.cpp
@@ -1,73 +1,93 @@
-#include <stdio.h> 
-#include <iostream> 
-#include <vector> 
-#include <queue> 
+#include<stdio.h> 
+#include<iostream> 
+#include<queue> 
+#include<vector> 
+#include<set> 
 
 #define MAXT 5
 #define MAXS 50000
 
 using namespace std; 
 
-typedef pair<long long, pair<int, int> > pli; 
+typedef pair<long long, int > pi; 
+typedef pair<long long, pair<int, int> > pii; 
+
+vector< pi > *graph = new vector<pi> [MAXS]; 
+set<int> cut; 
+
+long long cost[MAXS];
+int parent[MAXS]; 
+
+long long resp[MAXT]; 
 
 
-vector< pair<long long, int> > *graph = new vector<pair<long long, int > >[MAXS]; 
-priority_queue<pli, vector<pli>, greater<pli> > myQ; 
-int resp[MAXT]; 
-
-int rnk[MAXS], group[MAXS], parent[MAXS]; 
-
-int find (int el ) {
-	int dum = el ;
-
-	while ( dum != group[dum] ) 
-		dum = group[dum]; 
-
-	if ( el != dum )
-		group[el] = dum; 
-
-	return dum; 
-}
-
-void onion (int u, int v) {
-	int x = find(u), y=find(v);
-
-	if ( x != y ) { 
-		if (rnk[x] >= rnk[y]){
-			group[y] = x; 
-			if ( rnk[x] == rnk[y] )
-				rnk[x] += 1;
-		}	
-		else
-			parent[x] = y;
-	}
-} 
-
-int main() { 
-	int n,m,u,v ; 
-	long long w; 
-
-	scanf("%d %d",&n, &m); 
-
-	for(int i=0; i<m;i++) { 
-		scanf("%d %d %lld",&u, &v, &w);
-		myQ.push( {w, {u-1, v-1} } ); 
-		graph[u-1].push_back( {w, v-1} ) ; 
-		graph[v-1].push_back( {w, u-1} ); 
-	}
-
-	// algorithm goes here
-	
+void prim(int source, int size) { 
+	priority_queue< pii, vector<pii>, greater<pii> > myQ; 
 	int cnt = 0; 
-	while ( cnt != n-1) { 
-		u = myQ.top().second.first; v = myQ.top().second.second; 
+	pii help ; 
+	cost[source] = 0; 	
 
-		if (find(u) != find (v) )  
-			onion(u, v); 
+	myQ.push( { 0, {source, source } } ); 
+
+	while ( !myQ.empty() && cnt != size) { 
+		help = myQ.top(); 
+
+		cut.insert( help.second.first ) ;		//insert child into cut 
+		parent[help.second.first] = help.second.second; 
+		cost[help.second.first] = help.first; 		// < weight, child, parent >
+
+		myQ.pop(); 
+		
+		for(pi ady : graph[help.second.first] )  
+			if ( cut.find( ady.second ) == cut.end() )   // add edges with extremes not in cut to PQ
+				myQ.push( { ady.first, {ady.second, help.second.first} } ); 	
+			
 		cnt+=1; 
 	}
-	
-	//
+
+	cut.clear(); 
 }
 
 
+
+int main () { 
+	int t, n, m, u, v, k; 
+	long long w; 
+
+	scanf("%d",&t); 
+
+	k=t; 
+
+	while (k--) { 
+		scanf("%d %d", &n, &m); 
+
+		for(int i=0; i<n; i++) { cost[i] = 0; parent[i] = i; } //init
+
+		for(int i=0; i<m; i++) { 
+			scanf("%d %d %lld",&u,&v,&w); 
+			graph[u-1].push_back( {w,v-1} ); 	
+		}
+
+		// algorithm goes here
+
+		if ( m < n-1) 
+			resp[t-k-1] = -1; 
+		else {
+			prim(0, n); 	
+
+			int dum = n-1;
+			long long out; 
+			
+			while ( dum != 0 ) {  
+				out = min ( out, cost[dum] ) ;
+				dum = parent[dum]; 
+			}
+			resp[t-k-1] = out; 
+		}
+		//
+
+		for(int i=0; i<n; i++) graph[i].clear(); //clear
+	}
+
+	for(int i=0; i<t; i++) printf("%lld\n",resp[i]); 
+}
