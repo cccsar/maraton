c125ef4475bf1e039b5812f94605015111bce784
cccsar <15-11295@usb.ve>
2021-03-06 00:15:27 -0500

    added a template for segtree imp

---
+++ repaso/segtree.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h> 
+#include<vector> 
+
+using namespace std ;
+
+typedef vector<int> vi; 
+
+const int MAXN = 10000000; 
+
+vi st(4*MAXN, 0) ; 
+
+int create (int l, int r, int curr) { 
+    return 0; 
+}
+
+int update (int l, int r,int x , int e, int curr ) { 
+    return 0; 
+}
+
+int query (int l, int r, int p, int q, int curr) { 
+
+    return 0 ;
+}
+
+int main() { 
+   int n , t; 
+   vi arr; 
+
+   scanf("%d\n", &n); 
+
+   t = n; 
+
+   arr.resize(n); 
+        
+   while ( t-- )  
+       scanf("%d",&arr[n-t+1] );    
+
+   // test stuff here
+}
