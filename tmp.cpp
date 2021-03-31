92fa38f90f23738a3c3ff59b8b1255508815c0f8
cccsar <15-11295@usb.ve>
2021-03-30 20:07:47 -0400

    changed tmp a bit

---
+++ tmp.cpp
@@ -0,0 +1,40 @@
+/*
+ * 
+ *
+ */
+#include<stdio.h>
+#include<iostream> 
+#include<vector>
+#include<stack> 
+#include<queue> 
+#include<algorithm> 
+
+using namespace std; 
+
+int uwu;
+
+#define FOR(i,lb,ub) for(int i=lb ; i<ub ; ++i)
+#define ROF(i,ub,lb) for(int i=ub ; i>lb ; --i) 
+#define pb(el) push_back(el) 
+#define ri(i) scanf("%d",&i)
+#define rii(i,j) scanf("%d %d", &i, &j) 
+#define rl(l) scanf("%lld",&l)
+#define rll(l,m) scanf("%lld %lld",&l, &m) 
+#define SZ .size()
+#define fst first
+#define snd second
+
+const int MAXI = 0x7fffffff;
+
+typedef pair<int, int> pi ; 
+typedef pair<int, pi> pii; 
+typedef vector<long long> vl; 
+typedef vector<char> vc; 
+typedef vector<bool> vb; 
+typedef vector<int> vi; 
+typedef vector<pi> vii; 
+
+//GC
+
+int main() { 
+}
